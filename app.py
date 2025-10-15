9a5875bbeaead2df682d8b1c6a971b27237ec229
KeithMalvado <keithmalvado@gmail.com>
2025-10-15 09:17:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from PIL import Image
 import torch
 import torchvision.transforms as transforms
 import numpy as np
-from model_def import DnCNN
+from model_def import UNet
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @st.cache_resource
 def load_model():
-    model = DnCNN().to(device)
-    model.load_state_dict(torch.load("model_dncnn.pt", map_location=device))
+    model = UNet().to(device)
+    model.load_state_dict(torch.load("model_unet_dnoise.pt", map_location=device))
     model.eval()
     return model
 
@@ -21,17 +21,18 @@ transform = transforms.Compose([
 ])
 
 def denoise_image(model, image):
-    lr = transform(image).unsqueeze(0).to(device)
+    img_tensor = transform(image).unsqueeze(0).to(device)
     with torch.no_grad():
-        sr = model(lr).cpu().squeeze().permute(1, 2, 0).numpy()
-    sr = np.clip(sr, 0, 1)
-    return Image.fromarray((sr * 255).astype(np.uint8))
+        output = model(img_tensor).cpu().squeeze(0)
+    output_img = output.permute(1, 2, 0).numpy()
+    output_img = np.clip(output_img, 0, 1)
+    return Image.fromarray((output_img * 255).astype(np.uint8))
 
-st.set_page_config(page_title="Image Denoising", page_icon="✨", layout="centered")
+st.set_page_config(page_title="Image Denoising", layout="centered")
 
 st.markdown(
     """
-    <h2 style='text-align:center;'>Image Denoising using DnCNN</h2>
+    <h2 style='text-align:center;'>Image Denoising using UNet</h2>
     <p style='text-align:center;'>Upload citra buram atau penuh noise, dan model akan memulihkannya secara otomatis.</p>
     """,
     unsafe_allow_html=True
@@ -42,9 +43,7 @@ uploaded = st.file_uploader("Upload gambar", type=["jpg", "jpeg", "png"])
 if uploaded:
     img = Image.open(uploaded).convert("RGB")
     st.image(img, caption="Citra Asli (Noisy)", use_container_width=True)
-
     if st.button("Pulihkan Gambar"):
-        with st.spinner("Model sedang memulihkan citra... harap tunggu..."):
+        with st.spinner("Model sedang memulihkan citra..."):
             result = denoise_image(model, img)
         st.image(result, caption="Citra Setelah Denoising", use_container_width=True)
-        st.success("Proses denoising selesai")
