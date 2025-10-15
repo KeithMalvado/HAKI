b3c79eb2d8aae3240bea6b9705d55582bf3fab2e
KeithMalvado <keithmalvado@gmail.com>
2025-10-15 09:51:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @st.cache_resource
 def load_model():
-    model = torch.load("model_unet_dnoise.pt", map_location=device)
+    model = torch.load("model_unet_denoise.pt", map_location=device)
     model.eval()
     return model
 
 model = load_model()
+
